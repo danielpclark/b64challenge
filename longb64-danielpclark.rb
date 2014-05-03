9e2527e6461108dcca0710496e8d0ac9985ddca6
Daniel P. Clark <webmaster@6ftdan.com>
2014-05-03 18:39:33 -0400

    Update longb64-danielpclark.rb

--- longb64-danielpclark.rb
+++ longb64-danielpclark.rb
@@ -17,9 +17,9 @@ class B64
 	def inc(s)
 		# increment last item in string or false
 		unless @b64[s[-1]] == @b64.length-1
-			if first_non(s,s[-1]) # and not @b64[s[-2]] == @b64[s[-1]]+1
+			if first_non(s,s[-1])
 				s[-1] = first_non(s,s[-1])
-			elsif not first_non(s,s[-2]) # and not @b64[s[-2]] == @b64[s[-1]]+1
+			elsif not first_non(s,s[-2])
 				s[-1] = first_non(s)
 				if first_non(s,s[-2])
 					s[-2] = first_non(s,s[-2])
