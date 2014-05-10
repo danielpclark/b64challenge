e382cfeeed1eb4fce07aa93ce3da7cd191abd68d
Daniel P. Clark <webmaster@6ftdan.com>
2014-05-10 17:18:44 -0400

    Golf version submitted.

--- golfb64-danielpclark.rb
+++ golfb64-danielpclark.rb
@@ -1,18 +1,12 @@
-class B;def initialize(g)
-@g=g;@l=12;@b=(["A".."Z","a".."z","0".."9",["+","/"]].map{|z|Array z
-}.inject(:+)).each_with_index.map{|x,i|{x=>i}}.inject(:update)
-@i=@b.invert;@m=@l.times.map{|j|@i[@b.length-j-1]}.join;end
-def n;unless c(@g);r;end;@g;end;def c(s)
-unless @b[s[-1]]==@b.length-1;unless s==@m;if f(s,s[-1]);s[-1]=f(s,s[-1])
-elsif not f(s,s[-2]);s[-1]=f(s);if f(s,s[-2]);s[-2]=f(s,s[-2])
-else r end;else r end;else r end;else false end; true end
-def h(s);v=s.split("").map{|x|@b[x]};v.index(v.compact.max)end
-def r;if @g==@m;@g=@l.times.map{|j|@i[j]}.join;else
-v=@g[0..h(@g)-1];c(v);u=@g[h(@g)..-1].length;u.times{v+=f(v)}
-@g=v;end;end;def f(s,a = @i[0]);(@b[a]..@b.length-1).each { |p|
-unless !!s[@i[p]];return @i[p];end};false;end;end
+class B;def initialize(g)@l=12;@b=(("A".."Z").to_a+("a".."z").to_a+("0".."9").to_a+["+","/"]
+).each_with_index.map{|x,i|{x=>i}}.inject(:update);@g=g;@i=@b.invert;@m=@l.times.map{|i|
+@i[@b.length-i-1]}.join;end;def n;c(@g)?@g:r;end;def c(s);@b[s[-1]]==@b.length-1?(return false
+):s==@m?r: f(s,s[-1]) ?s[-1]=f(s,s[-1]):f(s,s[-2])?r: (s[-1]=f(s);f(s,s[-2])?s[-2]=f(s,s[-2]):r)
+true;end;def h(s)v=s.split("").map{|x|@b[x]};v.index(v.compact.max)end;def r;@g==@m?@g=@l.times.map{
+|j| @i[j]}.join: (v=@g[0..h(@g)-1];c(v);u=@g[h(@g)..-1].length;u.times{v+=f(v)};@g=v);end
+def f(s,a=@i[0])(@b[a]..@b.length-1).each{|p|unless !!s[@i[p]];return @i[p]end};false;end;end
 
-# Whitled down to 744 Characters
+# Whitled down to 658 Characters
 
 # Pass test cases
 puts "      IN   *******   OUT"
@@ -20,6 +14,6 @@ puts "      IN   *******   OUT"
 "ABCD/9876543","ABCD/987654+","ABCD/98765+E","ABCDEF+/9875","ABCDEF+/9876",
 "ABCDEF/GHIJK","ABCDEF/GHIJL","ABCDEFG+/987","A+/987654321","/+9876543210"
 	].each do |v|
-	t = B.new(v)
+	t = B.new(v.dup)
 	puts " #{v}   #{t.n}"
 end
