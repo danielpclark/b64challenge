ed207bed76b5c280a77c1cc169a55d0ba49e44d2
Daniel P. Clark <webmaster@6ftdan.com>
2014-05-10 15:20:10 -0400

    Handled edge case for maximum number adaptation.

--- longb64-danielpclark.rb
+++ longb64-danielpclark.rb
@@ -1,9 +1,11 @@
 class B64
-	def initialize(string)
+	def initialize(string, lngth=12)
 		@b64 = (
 			("A".."Z").to_a + ("a".."z").to_a + ("0".."9").to_a + ["+","/"]
 		).each_with_index.map {|x,i| {x=>i} }.inject(:update)
 		@string = string
+		@max = lngth.times.map { |i| @b64.invert[@b64.length-i-1] }.join("")
+		@start = lngth.times.map { |i| @b64.invert[i] }.join("")
 	end
 
 	def next
@@ -17,12 +19,16 @@ class B64
 	def inc(s)
 		# increment last item in string or false
 		unless @b64[s[-1]] == @b64.length-1
-			if first_non(s,s[-1])
-				s[-1] = first_non(s,s[-1])
-			elsif not first_non(s,s[-2])
-				s[-1] = first_non(s)
-				if first_non(s,s[-2])
-					s[-2] = first_non(s,s[-2])
+			unless s == @max
+				if first_non(s,s[-1])
+					s[-1] = first_non(s,s[-1])
+				elsif not first_non(s,s[-2])
+					s[-1] = first_non(s)
+					if first_non(s,s[-2])
+						s[-2] = first_non(s,s[-2])
+					else
+						swapper
+					end
 				else
 					swapper
 				end
@@ -42,14 +48,18 @@ class B64
 	end
 
 	def swapper
-		# save loop cycles by substituting with next unique charcter set
-		val = @string[0..seek_high(@string)-1]
-		inc(val)
-		count = @string[seek_high(@string)..-1].length
-		count.times do |pos|
-			val += first_non(val)
+		if @string == @max
+			@string = @start
+		else
+			# save loop cycles by substituting with next unique charcter set
+			val = @string[0..seek_high(@string)-1]
+			inc(val)
+			count = @string[seek_high(@string)..-1].length
+			count.times do
+				val += first_non(val)
+			end
+			@string = val
 		end
-		@string = val
 	end
 
 	def first_non(s,start = @b64.invert[0])
@@ -61,7 +71,6 @@ class B64
 		end
 		return false
 	end
-
 end
 
 # Pass 5 test cases with rollovers
